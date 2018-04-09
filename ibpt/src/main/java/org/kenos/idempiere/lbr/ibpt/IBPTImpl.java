5740901bee107c2a6b6bc2b60ca1db8349e46cdb
Marcus Garducci <mgarducci@kenos.com.br>
2018-04-09 16:09:32 -0300

    Com base no novo modelo da API, adicionei novos paramêtros que agora são obrigatórios e atualizei o endereço da chamada API.

--- ibpt/src/main/java/org/kenos/idempiere/lbr/ibpt/IBPTImpl.java
+++ ibpt/src/main/java/org/kenos/idempiere/lbr/ibpt/IBPTImpl.java
@@ -1,5 +1,6 @@
 package org.kenos.idempiere.lbr.ibpt;
 
+import java.math.BigDecimal;
 import java.net.URLEncoder;
 import java.util.Collections;
 
@@ -27,9 +28,9 @@ public class IBPTImpl implements IBPT
 	private ObjectMapper m_Mapper;
 	private final String m_Token;
 	//
-	private static final String URL 					= "http://iws.ibpt.org.br";
-	private static final String PRODUCTS	 			= "/api/deolhonoimposto/Produtos";
-	private static final String SERVICES	 			= "/api/deolhonoimposto/Servicos";
+	private static final String URL 					= "https://apidoni.ibpt.org.br";
+	private static final String PRODUCTS	 			= "/api/v1/produtos";
+	private static final String SERVICES	 			= "/api/v1/servicos";
 
 	private static final String ENCODING 				= "UTF-8";
 	
@@ -54,6 +55,18 @@ public class IBPTImpl implements IBPT
 	}	//	Pipedrive
 	
 	/**
+	 * Este era o método utilizado na versão antiga da API, em que alguns
+	 * campos não eram obrigatórios.
+	 * Veja o novo método, {@link IBPTimpl#getTaxForNCM(String, String, String, String,
+	 * String, String, BigDecimal, String)}
+	 */
+	@Deprecated
+	public IBPTResponseImpl getTaxForNCM (String cnpj, String ncm, String uf, String ex) throws Exception
+	{
+		return getTaxForNCM(cnpj, ncm, uf, ex, "", "", new BigDecimal(0), "");
+	}
+	
+	/**
 	 * 		Get tax for NCM
 	 * 
 	 * @param cnpj
@@ -63,13 +76,18 @@ public class IBPTImpl implements IBPT
 	 * @return
 	 * @throws Exception 
 	 */
-	public IBPTResponseImpl getTaxForNCM (String cnpj, String ncm, String uf, String ex) throws Exception
+	public IBPTResponseImpl getTaxForNCM (String cnpj, String ncm, String uf, String ex, String descricao, 
+			String unidadeMedida, BigDecimal valor, String gtin) throws Exception
 	{
 		String url = URL + PRODUCTS + m_Token
 						+ "&cnpj=" 		+ URLEncoder.encode (cnpj, ENCODING)
 						+ "&codigo=" 	+ URLEncoder.encode (ncm, ENCODING)
 						+ "&uf=" 		+ URLEncoder.encode (uf, ENCODING)
-						+ "&ex=" 		+ URLEncoder.encode (ex, ENCODING);
+						+ "&ex=" 		+ URLEncoder.encode (ex, ENCODING)
+						+ "&descricao="		+ URLEncoder.encode (descricao, ENCODING)
+						+ "&unidadeMedida="	+ URLEncoder.encode(unidadeMedida, ENCODING)
+						+ "&valor="			+ URLEncoder.encode (valor.toEngineeringString(), ENCODING)
+						+ "&gtin="			+ URLEncoder.encode (gtin, ENCODING);		
 		
 		Response response = WebClient
 				
@@ -93,6 +111,17 @@ public class IBPTImpl implements IBPT
 	}	//	getTaxForNCM
 	
 	/**
+	 * Este era o método utilizado na versão antiga da API, em que alguns
+	 * campos não eram obrigatórios.
+	 * Veja o novo método, {@link IBPTimpl#getTaxForNBS(String, String, String, String,
+	 * String, BigDecimal)}
+	 */
+	@Deprecated
+	public IBPTResponseImpl getTaxForNBS (String cnpj, String nbs, String uf) throws Exception
+	{
+		return getTaxForNBS(cnpj, nbs, uf, "", "", new BigDecimal(0));
+	}
+	/**
 	 * 	Get tax for NBS
 	 * 
 	 * @param cnpj
@@ -102,12 +131,16 @@ public class IBPTImpl implements IBPT
 	 * @return
 	 * @throws Exception 
 	 */
-	public IBPTResponseImpl getTaxForNBS (String cnpj, String nbs, String uf) throws Exception
+	public IBPTResponseImpl getTaxForNBS (String cnpj, String nbs, String uf, String descricao,
+			String unidadeMedida, BigDecimal valor) throws Exception
 	{
 		String url = URL + SERVICES + m_Token
 						+ "&cnpj=" 		+ URLEncoder.encode (cnpj, ENCODING)
 						+ "&codigo=" 	+ URLEncoder.encode (nbs, ENCODING)
-						+ "&uf=" 		+ URLEncoder.encode (uf, ENCODING);
+						+ "&uf=" 		+ URLEncoder.encode (uf, ENCODING)
+						+ "&descricao=" + URLEncoder.encode (descricao, ENCODING)
+						+ "&unidadeMedida=" + URLEncoder.encode(unidadeMedida, ENCODING)
+						+ "&valor=" 	+ URLEncoder.encode(valor.toEngineeringString(), ENCODING);
 		
 		Response response = WebClient
 				
@@ -133,15 +166,19 @@ public class IBPTImpl implements IBPT
 	public static void main (String[] args) throws Exception
 	{
 		String key 	= "qwertyuiopasdfghjklzxcvbnm";
-		String cnpj = "00000000000000";
+		String cnpj = "00000000000000"; //Remove the special characters, like . - /
 		String ncm 	= "101011000";
 		String nbs 	= "38249031";
 		String uf  	= "SP";
 		String ex  	= "0";
+		String descricao	 = "free";
+		String unidadeMedida = "metro";
+		BigDecimal valor 	 = new BigDecimal(39.99);
+		String gtin			 = "7894900530001";
 		
 		IBPTImpl ibpt = new IBPTImpl (key);
 		//
-		IBPTResponseImpl taxForNCM = ibpt.getTaxForNCM(cnpj, ncm, uf, ex);
+		IBPTResponseImpl taxForNCM = ibpt.getTaxForNCM(cnpj, ncm, uf, ex, descricao, unidadeMedida, valor, gtin);
 		
 		if (taxForNCM != null && taxForNCM.getCodigo() != null)
 		{
@@ -160,7 +197,7 @@ public class IBPTImpl implements IBPT
 		else
 			System.out.println("null response");
 		
-		IBPTResponseImpl taxForNBS = ibpt.getTaxForNBS(cnpj, nbs, uf);
+		IBPTResponseImpl taxForNBS = ibpt.getTaxForNBS(cnpj, nbs, uf, descricao, unidadeMedida, valor);
 		
 		if (taxForNBS != null && taxForNBS.getCodigo() != null)
 		{
